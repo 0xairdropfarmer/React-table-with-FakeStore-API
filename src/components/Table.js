66bc3adc329f7c435ebc220b4a2a0e54f48322d0
krissanawat101 <krissanawat101@gmail.com>
2021-02-12 17:03:46 +0700

    part 4 done

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,28 +1,114 @@
 import React, { useState } from "react";
-import { useTable, useFilters, useSortBy } from "react-table";
-
+import {
+  useTable,
+  useFilters,
+  useSortBy,
+  useGroupBy,
+  useExpanded,
+  usePagination
+} from "react-table";
+import "./Table.css";
+import { useExportData } from "react-table-plugins";
+import Papa from "papaparse";
+import XLSX from "xlsx";
+import JsPDF from "jspdf";
+import "jspdf-autotable";
 export default function Table({ columns, data }) {
   const [filterInput, setFilterInput] = useState("");
   const {
     getTableProps,
     getTableBodyProps,
     headerGroups,
-    rows,
+    prepareRow,
     setFilter,
-    prepareRow
+    page,
+    canPreviousPage,
+    canNextPage,
+    pageOptions,
+    pageCount,
+    gotoPage,
+    nextPage,
+    previousPage,
+    setPageSize,
+    state: { pageIndex, pageSize },
+    // for export
+    exportData
   } = useTable(
     {
       columns,
-      data
+      data,
+      getExportFileBlob
     },
-    useFilters,useSortBy
+    useFilters,
+    useGroupBy,
+    useSortBy,
+    useExpanded,
+    usePagination,
+    useExportData
   );
   const handleFilterChange = (e) => {
     const value = e.target.value || undefined;
     //setFilter("name", value);
-    setFilter("title", value);
+    setFilter("alias", value);
     setFilterInput(value);
   };
+  function getExportFileBlob({ columns, data, fileType, fileName }) {
+    if (fileType === "csv") {
+      // CSV example
+      const headerNames = columns
+        .filter((c) => c.Header != "Action")
+        .map((col) => col.exportValue);
+      const csvString = Papa.unparse({ fields: headerNames, data });
+      return new Blob([csvString], { type: "text/csv" });
+    } else if (fileType === "xlsx") {
+      // XLSX example
+
+      const header = columns
+        .filter((c) => c.Header != "Action")
+        .map((c) => c.exportValue);
+      const compatibleData = data.map((row) => {
+        const obj = {};
+        header.forEach((col, index) => {
+          obj[col] = row[index];
+        });
+        return obj;
+      });
+
+      let wb = XLSX.utils.book_new();
+      let ws1 = XLSX.utils.json_to_sheet(compatibleData, {
+        header
+      });
+      XLSX.utils.book_append_sheet(wb, ws1, "React Table Data");
+      XLSX.writeFile(wb, `${fileName}.xlsx`);
+
+      // Returning false as downloading of file is already taken care of
+      return false;
+    }
+    //PDF example
+    if (fileType === "pdf") {
+      const headerNames = columns
+        .filter((c) => c.Header != "Action")
+        .map((column) => column.exportValue);
+      const doc = new JsPDF();
+      doc.autoTable({
+        head: [headerNames],
+        body: data,
+        styles: {
+          minCellHeight: 9,
+          halign: "left",
+          valign: "center",
+          fontSize: 11
+        }
+      });
+      doc.save(`${fileName}.pdf`);
+
+      return false;
+    }
+
+    // Other formats goes here
+    return false;
+  }
+  // Render the UI for your table
   return (
     <>
       <div className="form-group input-group">
@@ -32,6 +118,33 @@ export default function Table({ columns, data }) {
           onChange={handleFilterChange}
           placeholder={"Search name"}
         />
+      </div>
+      <div className="form-group input-group">
+        <button
+          class="btn btnexport mr-1"
+          onClick={() => {
+            exportData("csv", true);
+          }}
+        >
+          <i class="fa fa-file-csv"></i> Export as CSV
+        </button>{" "}
+        <button
+          class="btn btnexport mr-1"
+          onClick={() => {
+            exportData("xlsx", true);
+          }}
+        >
+          <i class="fa fa-file-excel"></i> Export as xlsx
+        </button>{" "}
+        <button
+          class="btn btnexport mr-1"
+          onClick={() => {
+            exportData("pdf", true);
+          }}
+        >
+          <i class="fa fa-file-pdf"></i>
+          Export as PDF
+        </button>
       </div>
       <table
         {...getTableProps()}
@@ -43,6 +156,12 @@ export default function Table({ columns, data }) {
             <tr {...headerGroup.getHeaderGroupProps()}>
               {headerGroup.headers.map((column) => (
                 <th {...column.getHeaderProps(column.getSortByToggleProps())}>
+                  {column.canGroupBy ? (
+                    // If the column can be grouped, let's add a toggle
+                    <span {...column.getGroupByToggleProps()}>
+                      {column.isGrouped ? "🛑 " : "👊 "}
+                    </span>
+                  ) : null}
                   {column.render("Header")}
                   <span>
                     {column.isSorted
@@ -57,13 +176,31 @@ export default function Table({ columns, data }) {
           ))}
         </thead>
         <tbody {...getTableBodyProps()}>
-          {rows.map((row, i) => {
+          {page.map((row, i) => {
             prepareRow(row);
             return (
               <tr {...row.getRowProps()}>
                 {row.cells.map((cell) => {
                   return (
-                    <td {...cell.getCellProps()}>{cell.render("Cell")}</td>
+                    <td {...cell.getCellProps()}>
+                      {" "}
+                      {cell.isGrouped ? (
+                        // If it's a grouped cell, add an expander and row count
+                        <>
+                          <span {...row.getToggleRowExpandedProps()}>
+                            {row.isExpanded ? "👇" : "👉"}
+                          </span>{" "}
+                          {cell.render("Cell")} ({row.subRows.length})
+                        </>
+                      ) : cell.isAggregated ? (
+                        // If the cell is aggregated, use the Aggregated
+                        // renderer for cell
+                        cell.render("Aggregated")
+                      ) : cell.isPlaceholder ? null : ( // For cells with repeated values, render null
+                        // Otherwise, just render the regular cell
+                        cell.render("Cell")
+                      )}
+                    </td>
                   );
                 })}
               </tr>
@@ -71,6 +208,75 @@ export default function Table({ columns, data }) {
           })}
         </tbody>
       </table>
+      <form className="inline">
+        <div className="form-row">
+          <div className="form-group input-group col-md-2">
+            <ul className="pagination">
+              <li
+                class={!canPreviousPage ? "page-item disabled" : "page-item "}
+              >
+                <a className="page-link" onClick={() => gotoPage(0)}>
+                  {"<<"}
+                </a>
+              </li>
+              <li
+                class={!canPreviousPage ? "page-item disabled" : "page-item "}
+              >
+                <a className="page-link" onClick={() => previousPage()}>
+                  {"<"}
+                </a>
+              </li>
+              <li class={!canNextPage ? "page-item disabled" : "page-item "}>
+                <a className="page-link" onClick={() => nextPage()}>
+                  {">"}
+                </a>
+              </li>
+              <li class={!canNextPage ? "page-item disabled" : "page-item "}>
+                <a
+                  className="page-link"
+                  onClick={() => gotoPage(pageCount - 1)}
+                >
+                  {">>"}
+                </a>
+              </li>
+            </ul>
+          </div>
+          <div className="form-group input-group col-md-2">
+            <input
+              className="form-control"
+              type="number"
+              defaultValue={pageIndex + 1}
+              onChange={(e) => {
+                const page = e.target.value ? Number(e.target.value) - 1 : 0;
+                gotoPage(page);
+              }}
+              style={{ width: "100px" }}
+            />
+          </div>
+          <div className="form-group input-group col-md-2">
+            <select
+              className="custom-select"
+              value={pageSize}
+              onChange={(e) => {
+                setPageSize(Number(e.target.value));
+              }}
+            >
+              {[10, 20, 30, 40, 50].map((pageSize) => (
+                <option key={pageSize} value={pageSize}>
+                  Show {pageSize}
+                </option>
+              ))}
+            </select>
+          </div>
+          <span>
+            Page{" "}
+            <strong>
+              {pageIndex + 1} of {pageOptions.length}
+            </strong>{" "}
+            | Go to page:{" "}
+          </span>
+        </div>
+      </form>
     </>
   );
 }
